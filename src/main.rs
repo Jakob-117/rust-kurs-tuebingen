59b7a7b396581d10c65a4986e8fe4060e7f3f97e
Jakob-117 <pohl.jakob@protonmail.com>
2023-04-15 20:11:27 +0200

    unwrap_or_lesson with args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    
+    extra_func();
+}
+
+fn extra_func(){
+    let x = std::env::args()
+    .nth(1)
+    .map(|s: String| {
+        i32::from_str(&s)
+    })
+    .unwrap_or(Ok(0))
+    .unwrap_or_else(|err|{
+        println!("Error while parsing: {}", err);
+        0
+    });
+
+    for i in x..10{
+        println!("{i}");
+    }
 }
