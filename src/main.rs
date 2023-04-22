da2dec3cfc64b07bfa89350b884609c0dff750f0
Jakob-117 <pohl.jakob@protonmail.com>
2023-04-22 16:18:47 +0200

    bmi_calc refactored
    made functions for the inputs and the calculation of the bmi

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,53 @@
 use std::env::args;
-use std::io;
+use std::io::{self, Stdin};
 use std::str::FromStr;
 use std::io::Write;
 
+
 fn main() {
     let stdin = std::io::stdin();
+    get_weight(stdin)
+    
+}
+
+
+fn get_weight(input: Stdin){
+    
     print!("Gebe dein Gewicht in kg ein: ");
     let _ = std::io::stdout().flush(); //flush is to really print the stuff
 
     let mut buffer_weight = String::new();
-    stdin.read_line(&mut buffer_weight).unwrap();
+    input.read_line(&mut buffer_weight).unwrap();
     println!(" ");
 
     let weight = f64::from_str(buffer_weight.trim()).unwrap(); //trim() removed das trailing "enter" also \n was durch das enter drücken mit kommt -> \n kann nicht geparset werden
+    
 
     println!("Weigth: {weight}");
+    get_height(input ,weight);
+    
+}
 
-    let stdin = std::io::stdin();
+
+fn get_height(input: Stdin, weight: f64){
+
     print!("Gebe deine Höhe in Meter ein: ");
     let _ = std::io::stdout().flush(); //to really print the stuff
 
     let mut buffer_height = String::new();
-    stdin.read_line(&mut buffer_height).unwrap();
+    input.read_line(&mut buffer_height).unwrap();
     println!(" ");
 
     let height = f64::from_str(buffer_height.trim()).unwrap();
 
     println!("Heigth: {height}");
-
-    let bmi = weight / (height * height);
-    println!("BMI: {bmi}");
+    bmi_calc(height, weight);
 }
 
 
+
+fn bmi_calc(height: f64, weight: f64) -> f64{
+    let bmi = weight / (height * height);
+    println!("BMI: {bmi}");
+    bmi
+}
