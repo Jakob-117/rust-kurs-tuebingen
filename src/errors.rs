5c6ab464cbc74a8b2bbdc89d6b31f28e56dc558d
Jakob-117 <pohl.jakob@protonmail.com>
2023-05-06 17:51:23 +0200

    Made test, outsorced structs etc
    Made tests for bmi calculations.
    Outsorced every structs and enums

---
+++ src/errors.rs
@@ -0,0 +1,11 @@
+#[derive(Debug, PartialEq)]
+pub enum BmiError{
+    HeightIsZero,
+    HeightIsNegative,
+    WeightIsNotOk
+}
+
+#[derive(Debug)]
+pub enum InputError{
+    //InputIsntStd
+}
---
+++ src/weight.rs
@@ -0,0 +1,1 @@
+pub struct Weight(pub f64); //8 Byte groß, dank Zero cost abstractions
